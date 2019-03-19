375971953d2fc50d5f094e88764d37cbdfeeb763
matusca96 <gomesloud@gmail.com>
2019-03-19 18:28:02 -0300

    Cadastro de IPI.

---
+++ webapp/controller/CstIpi.controller.js
@@ -0,0 +1,161 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function(Controller, MessageBox) {
+	"use strict";
+
+	return Controller.extend("br.com.idxtecCstIpi.controller.CstIpi", {
+		onInit: function(){
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		},
+	
+		onRefresh: function(){
+			var oModel = this.getOwnerComponent().getModel();
+			oModel.refresh(true);
+		},
+		
+		onIncluir: function(){
+			var oDialog = this._criarDialog();
+			var oModel = this.getOwnerComponent().getModel();
+			var oViewModel = this.getModel("view");
+		
+			oViewModel.setData({
+				titulo: "Incluir novo CST - IPI",
+				codigoEdit: true,
+				msgSalvar: "CST - IPI inserido com sucesso!"
+			});
+			
+			oDialog.unbindElement();
+			oDialog.setEscapeHandler(function(oPromise){
+				if(oModel.hasPendingChanges()){
+					oModel.resetChanges();
+				}
+			});
+			
+			var oContext = oModel.createEntry("/CstIpis", {
+				properties: {
+					"Codigo": "",
+					"Descricao": ""
+				}
+			});
+			
+			oDialog.setBindingContext(oContext);
+			oDialog.open();
+		},
+		
+		onEditar: function(){
+			var oDialog = this._criarDialog();
+			var oTable = this.byId("tableCstIpi");
+			var nIndex = oTable.getSelectedIndex();
+			var oViewModel = this.getModel("view");
+			
+			oViewModel.setData({
+				titulo: "Editar CST - IPI",
+				codigoEdit: false,
+				msgSalvar: "CST - IPI alterado com sucesso!"
+			});
+			
+			if(nIndex === -1){
+				MessageBox.information("Selecione um CST - IPI da tabela!");
+				return;
+			}
+			
+			var oContext = oTable.getContextByIndex(nIndex);
+			
+			oDialog.bindElement(oContext.sPath);
+			oDialog.open();
+		},
+		
+		onRemover: function(){
+			var that = this;
+			var oTable = this.byId("tableCstIpi");
+			var nIndex = oTable.getSelectedIndex();
+			
+			if(nIndex === -1){
+				MessageBox.information("Selecione um CST - IPI da tabela!");
+				return;
+			}
+			
+			MessageBox.confirm("Deseja remover esse CST - IPI?", {
+				onClose: function(sResposta){
+					if(sResposta === "OK"){
+						that._remover(oTable, nIndex);
+						MessageBox.success("CST - IPI removido com sucesso!");
+					}
+				} 
+			});
+		},
+		
+		onSaveDialog: function(){
+			var oView = this.getView();
+			var oModel = this.getOwnerComponent().getModel();
+			var oViewModel = this.getModel("view");
+			
+			if(this._checarCampos(this.getView()) === true){
+				MessageBox.information("Preencha todos os campos obrigatórios!");
+				return;
+			} else{
+				oModel.submitChanges({
+					success: function(){
+						oModel.refresh(true);
+						MessageBox.success(oViewModel.getData().msgSalvar);
+						oView.byId("CstIpiDialog").close();
+						oView.byId("tableCstIpi").clearSelection();
+					},
+					error: function(oError){
+						var sError = JSON.parse( oError.responseText).error.message.value; 
+						MessageBox.error(sError);
+					}
+				});
+			}
+		},
+		
+		onCloseDialog: function(){
+			var oModel = this.getOwnerComponent().getModel();
+			
+			if(oModel.hasPendingChanges()){
+				oModel.resetChanges();
+			}
+			
+			this.byId("CstIpiDialog").close();
+		},
+		
+		_remover: function(oTable, nIndex){
+			var oModel = this.getOwnerComponent().getModel();
+			var oContext = oTable.getContextByIndex(nIndex);
+			
+			oModel.remove(oContext.sPath, {
+				success: function(){
+					oModel.refresh(true);
+					oTable.clearSelection();
+				},
+				error: function(oError){
+					MessageBox.error(oError.responseText);
+				}
+			});
+		},
+		
+		_criarDialog: function(){
+			var oView = this.getView();
+			var oDialog = this.byId("CstIpiDialog");
+			
+			if(!oDialog){
+				oDialog = sap.ui.xmlfragment(oView.getId(), "br.com.idxtecCstIpi.view.CstIpiDialog", this);
+				oView.addDependent(oDialog); 
+			}
+			return oDialog;
+		},
+		
+		_checarCampos: function(oView){
+			if(oView.byId("codigo").getValue() === "" || oView.byId("descricao").getValue() === ""){
+				return true;
+			} else{
+				return false; 
+			}
+		},
+		
+		getModel: function(sModel) {
+			return this.getOwnerComponent().getModel(sModel);
+		}
+	});
+});
